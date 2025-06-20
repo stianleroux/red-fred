6119d9fb7bfc32fc86400ab6e46847f694a35a2a
Stian Le Roux <stianleroux@gmail.com>
2025-06-20 15:05:16 +0200

    🎨 Add tailwindcss and 🗒️ scripts/zod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [],
+    vite: {
+        resolve: {
+            alias: {
+                '@scripts': path.resolve('./src/scripts'),
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                // ...existing code...
+            },
+        },
+    },
+});
